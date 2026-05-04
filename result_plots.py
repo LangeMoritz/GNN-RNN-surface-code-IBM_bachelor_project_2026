05c639dc1ca3f197ccd05ca8af9b5b903c3ffe18
Felicia Johanson <feliciajohanson63@gmail.com>
2026-05-04 21:43:34 +0200

    Updated plots file according to new results

--- result_plots.py
+++ result_plots.py
@@ -14,45 +14,51 @@ plt.rcParams.update({
 # 1. Training curves
 # =========================
 
-d3_dem = np.load("jobs/stats/finetune_dem_real_phaseA.npy")
-d3_real = np.load("jobs/stats/finetune_dem_real_phaseB.npy")
+d3_dem_t10 = np.load("jobs/stats/finetune_dem_real_phaseA.npy")
+d3_real_t10 = np.load("jobs/stats/finetune_dem_real_phaseB.npy")
+d3_dem_t20 = np.load("jobs/stats/finetune_dem_real_phaseA_T20.npy")
+d3_real_t20 = np.load("jobs/stats/finetune_dem_real_phaseB_T20.npy")
 
-# Lägg in när d=5 finns
-# d5_dem = np.load("jobs/stats/finetune_d5_dem_phaseA.npy")
-# d5_real = np.load("jobs/stats/finetune_d5_real_phaseB.npy")
+d5_dem_t10 = np.load("jobs/stats/finetune_real_t10_B.npy")
+d5_real_t10 = np.load("jobs/stats/finetune_real_t10_C.npy")
 
 # Rad 4 = train accuracy
 # Rad 10 = validation accuracy
 
-d3_dem_train = 1 - d3_dem[4]
-d3_dem_val = 1 - d3_dem[10]
+d3_dem_train_t10 = 1 - d3_dem_t10[4]
+d3_dem_val_t10 = 1 - d3_dem_t10[10]
+d3_dem_train_t20 = 1 - d3_dem_t20[4]
+d3_dem_val_t20 = 1 - d3_dem_t20[10]
 
-d3_real_train = 1 - d3_real[4]
-d3_real_val = 1 - d3_real[10]
+d3_real_train_t10 = 1 - d3_real_t10[4]
+d3_real_val_t10 = 1 - d3_real_t10[10]
+d3_real_train_t20 = 1 - d3_real_t20[4]
+d3_real_val_t20 = 1 - d3_real_t20[10]
 
-# Lägg in senare
-# d5_dem_train = 1 - d5_dem[4]
-# d5_dem_val = 1 - d5_dem[10]
+d5_dem_train_t10 = 1 - d5_dem_t10[4]
+d5_dem_val_t10 = 1 - d5_dem_t10[10]
 
-# d5_real_train = 1 - d5_real[4]
-# d5_real_val = 1 - d5_real[10]
+d5_real_train_t10 = 1 - d5_real_t10[4]
+d5_real_val_t10 = 1 - d5_real_t10[10]
 
 
 # =========================
 # DEM training plot
 # =========================
 
-epochs_d3 = np.arange(1, len(d3_dem_train) + 1)
+epochs_d3 = np.arange(1, len(d3_dem_train_t10) + 1)
 
 plt.figure(figsize=(8, 5))
 
-plt.plot(epochs_d3, d3_dem_train, linewidth=2, color="darkblue", label="d=3 Träning")
-plt.plot(epochs_d3, d3_dem_val, "--", linewidth=2, color="darkblue", label="d=3 Validering")
+plt.plot(epochs_d3, d3_dem_train_t10, linewidth=2, color="darkblue", label="d=3 T=10 Träning")
+plt.plot(epochs_d3, d3_dem_val_t10, "--", linewidth=2, color="darkblue", label="d=3 T=10 Validering")
+plt.plot(epochs_d3, d3_dem_train_t20, linewidth=2, color="seagreen", label="d=3 T=20 Träning")
+plt.plot(epochs_d3, d3_dem_val_t20, "--", linewidth=2, color="seagreen", label="d=3 T=20 Validering")
 
 # Lägg in senare:
-# epochs_d5 = np.arange(1, len(d5_dem_train) + 1)
-# plt.plot(epochs_d5, d5_dem_train, linewidth=2, color="darkgreen", label="d=5 Träning")
-# plt.plot(epochs_d5, d5_dem_val, "--", linewidth=2, color="darkgreen", label="d=5 Validering")
+epochs_d5 = np.arange(1, len(d5_dem_train_t10) + 1)
+plt.plot(epochs_d5, d5_dem_train_t10, linewidth=2, color="darkgreen", label="d=5 T=10 Träning")
+plt.plot(epochs_d5, d5_dem_val_t10, "--", linewidth=2, color="darkgreen", label="d=5 T=10 Validering")
 
 plt.xlabel("Epok")
 plt.ylabel("Logisk felfrekvens")
@@ -68,17 +74,18 @@ plt.show()
 # Real-data fine-tuning plot
 # =========================
 
-epochs_d3 = np.arange(1, len(d3_real_train) + 1)
+epochs_d3 = np.arange(1, len(d3_real_train_t10) + 1)
 
 plt.figure(figsize=(8, 5))
 
-plt.plot(epochs_d3, d3_real_train, linewidth=2, color="darkblue", label="d=3 Träning")
-plt.plot(epochs_d3, d3_real_val, "--", linewidth=2, color="darkblue", label="d=3 Validering")
+plt.plot(epochs_d3, d3_real_train_t10, linewidth=2, color="darkblue", label="d=3 T=10 Träning")
+plt.plot(epochs_d3, d3_real_val_t10, "--", linewidth=2, color="darkblue", label="d=3 T=10 Validering")
+plt.plot(epochs_d3, d3_real_train_t20, linewidth=2, color="seagreen", label="d=3 T=20 Träning")
+plt.plot(epochs_d3, d3_real_val_t20, "--", linewidth=2, color="seagreen", label="d=3 T=20 Validering")
 
-# Lägg in senare:
-# epochs_d5 = np.arange(1, len(d5_real_train) + 1)
-# plt.plot(epochs_d5, d5_real_train, linewidth=2, color="darkgreen", label="d=5 Träning")
-# plt.plot(epochs_d5, d5_real_val, "--", linewidth=2, color="darkgreen", label="d=5 Validering")
+epochs_d5 = np.arange(1, len(d5_real_train_t10) + 1)
+plt.plot(epochs_d5, d5_real_train_t10, linewidth=2, color="darkgreen", label="d=5 T=10 Träning")
+plt.plot(epochs_d5, d5_real_val_t10, "--", linewidth=2, color="darkgreen", label="d=5 T=10 Validering")
 
 plt.xlabel("Epok")
 plt.ylabel("Logisk felfrekvens")
@@ -104,23 +111,23 @@ labels = [
 # Fyll i värden när de kommer
 trained_gnn = [
     0.1352,   # d3 T10
-    np.nan,     # d3 T20
-    np.nan,     # d5 T10
+    0.2154,     # d3 T20
+    0.2393,     # d5 T10
     np.nan,     # d5 T20
 ]
 
 mwpm = [
     0.1844,   # d3 T10
     0.2618,   # d3 T20
-    np.nan,     # d5 T10
-    np.nan,     # d5 T20
+    0.3397,     # d5 T10
+    0.4465,     # d5 T20
 ]
 
 untrained_gnn = [
     0.2378,   # d3 T10
     0.3666,   # d3 T20
-    np.nan,     # d5 T10
-    np.nan,     # d5 T20
+    0.2791,     # d5 T10
+    0.4090,     # d5 T20
 ]
 
 x = np.arange(len(labels))
